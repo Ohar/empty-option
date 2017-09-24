ce68d1417efe23086200364c7c3c575d4e20f8de
Pavel Lysenko <code@ohar.name>
2017-09-24 05:45:38 +0300

    Styles: remove exceed .less file to fix module loading

--- index.jsx
+++ index.jsx
@@ -2,9 +2,8 @@
 // Prevent Firefox to show selected option when no option is selected in fact
 // https://bugzilla.mozilla.org/show_bug.cgi?id=46845
 
-import './styles.less';
 import React from 'react';
 
 export default function EmptyOption ({value = ''}) {
-	return <option className="EmptyOption" value={value}></option>
+	return <option style="display: none;" value={value}></option>
 }
